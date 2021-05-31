bf77eaa39f6c587d96ad5de6a6d6a9cc24d02694
Lostcr <cristianlealb2@gmail.com>
2021-05-31 19:09:25 -0400

    final cap 1 curso junior

--- Junior_JS/Cap-1/enlazar_js.js
+++ Junior_JS/Cap-1/enlazar_js.js
@@ -1,7 +1,49 @@
-function test(i){
-  if (i>1) {
-    console.log("hello world")
-  }else{
-    console.log("bye world")
-  }
-}
+/* function test(i){
+   if (i>1) {
+     console.log("hello world")
+   }else{
+     console.log("bye world")
+   }
+ }*/
+
+/*tipos de datos:
+String = "cadena de texto"'cadena de texto'`cadena de texto`
+number = 20
+booleano = true or false
+undefined = indica una variable que existe pero que no tiene ningun valor asignado.
+Null = es un tipo de dato que inicializa una variable y la declara su valor como nulo o vacio.
+Nan = not an number es lo que se obtine al tratar de reallizar una operacion matematica entre un tipo de dato string y un dato number.
+*/
+
+/*scope de variables:
+var = scope global
+let = scope en donde se declara
+const = es para guardar datos que no van a ser modificados en ningun punto del programa.ademas se debe inicialiar al momento de declararla.
+*/
+/*Hoisting:
+ hace referencia al comportamiento que se presenta al momento de llamar a una funcion antes de que esta sea escrita
+*/
+/*prompt
+let nombre = prompt('dime tu nombre');
+alert('Hola ' + nombre);
+*/
+
+/*operadores de asignacion y aritmeticos
+asignacion: =, +=, -=, *=, /=, %=, **=;
+aritmeticos: +, -, /, *, %, **, ++, --;
+*/
+/*concatenacion:
+  la concatenacion es la manera en la que se unen cadenas de textos almacenadas en diferentes variables.
+  una buena practica de concatenar cadenas de texto es usar ${} que se usan dentro de `` quedaria de la siguiente manera let nombre = "cristian";
+                      frase = `soy ${nombre}`
+*/
+/*operadores de comparacion
+ ==, !=, ===, !==, >, <, >=, <=;
+ estos operadores realizan una operacion y el valor que devuelven un dato de tipo boleano(true or false)
+*/
+/*operadores logicos
+ &&, ||, !;
+*/
+/*condicionales
+ if, else, else if;
+*/
