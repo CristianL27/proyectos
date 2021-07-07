78a8ee6ebd3ace0f6198d44e104d8759e8f11fb4
Lostcr <cristianlealb2@gmail.com>
2021-07-06 23:11:08 -0400

    resolucion del problema C

---
+++ historia_de_cofla/cap 2/problema3/codigo.js
@@ -0,0 +1,30 @@
+let operaciones = parseInt(prompt("ingrese el numero relacionado con su operacion: 1)Suma, 2)Resta, 3)Multiplicacion, 4)Divicion"))
+
+if (operaciones < 1 || operaciones > 4 ){
+  alert('Error el número seleccionado no corresponde con una operación válida')
+
+}else{
+  valor1 = parseInt(prompt("ingrese el primer numero"))
+  valor2 = parseInt(prompt("ingrese el segundo numero"))
+}
+
+function calculadora(num1, num2){
+
+  let resultado
+  if (operaciones === 1) {
+    operacion = 'Suma'
+    resultado = num1 + num2
+  }else if (operaciones === 2) {
+    operacion = 'Resta'
+    resultado = num1 - num2
+  } else if (operaciones === 3) {
+    operacion = 'Multiplicación'
+    resultado = num1 * num2
+  } else if (operaciones === 4) {
+    operacion = 'División'
+    resultado = num1 / num2
+  }
+
+ document.write(`El resultado de la ${operacion} es igual a ${resultado} `)
+}
+calculadora(valor1, valor2)
